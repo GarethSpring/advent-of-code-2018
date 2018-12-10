74f35ac61d1f18312c1be705ec1041fa2dcc945d
Gareth Spring <gareth.spring@gmail.com>
2018-12-10 21:53:19 +0000

    WIP Day 11

---
+++ advent-of-code-2018.Tests/Days/Day11/Day11Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using advent_of_code_2018.Days.Day11;
+
+namespace advent_of_code_2018.Tests.Days.Day11
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        [TestMethod]
+        public void Day10_TestPart1()
+        {
+            var solution = new Solution();
+
+            var result = solution.Part1();           
+        }
+    }
+}
---
+++ advent-of-code-2018/Days/Day11/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2018.Days.Day11
+{
+    public class Solution
+    {
+        public int Part1()
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
