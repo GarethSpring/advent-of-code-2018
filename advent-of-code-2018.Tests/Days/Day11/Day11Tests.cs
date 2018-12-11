32e5b170e311b4bef1f4fea27cd5d3dc90214341
Gareth Spring <gareth.spring@dunnhumby.com>
2018-12-11 14:25:12 +0000

    Day 11

--- advent-of-code-2018.Tests/Days/Day11/Day11Tests.cs
+++ advent-of-code-2018.Tests/Days/Day11/Day11Tests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using advent_of_code_2018.Days.Day11;
 
 namespace advent_of_code_2018.Tests.Days.Day11
@@ -7,11 +8,24 @@ namespace advent_of_code_2018.Tests.Days.Day11
     public class Day11Tests
     {
         [TestMethod]
-        public void Day10_TestPart1()
+        public void Day11_TestPart1()
         {
             var solution = new Solution();
 
-            var result = solution.Part1();           
+            var result = solution.Part1(); 
+            
+            Assert.IsTrue(result.Item1 == 19);
+            Assert.IsTrue(result.Item2 == 41);
+        }
+
+        [TestMethod]
+        public void Day11_TestPart2()
+        {
+            var solution = new Solution();
+
+            solution.Part2();       
+                        
+            // 237,284,11
         }
     }
 }
--- advent-of-code-2018/Days/Day11/Solution.cs
+++ advent-of-code-2018/Days/Day11/Solution.cs
@@ -1,18 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace advent_of_code_2018.Days.Day11
 {
     public class Solution
     {
-        public int Part1()
-        {
-            int result = 0;
+        private const int Input = 5535;
+        
+        private const int GridSize = 300;
 
-            return result;
+        private readonly Dictionary<string, FuelCell> cells = new Dictionary<string, FuelCell>();
+
+        private readonly int[,,] powerCache = new int[301,301,301];
+
+        public Tuple<int, int> Part1()
+        {            
+            InitCells();
+
+            int MaxPowerXCoord = 0;
+            int MaxPowerYCoord = 0;
+            int MaxPower = 0;
+
+            for (int testX = 1; testX < GridSize - 2; testX++)
+            {
+                for (int testY = 1; testY < GridSize - 2; testY++)
+                {
+                    int power = GetPowerLevel(testX, testY, 3);
+                    {
+                        if (power > MaxPower)
+                        {
+                            MaxPower = power;
+                            MaxPowerXCoord = testX;
+                            MaxPowerYCoord = testY;
+                        }
+                    }
+                }
+            }
+                      
+            return new Tuple<int, int>(MaxPowerXCoord, MaxPowerYCoord);
+        }
+
+        public void Part2()
+        {
+            InitCells();
+
+            int MaxPowerXCoord = 0;
+            int MaxPowerYCoord = 0;
+            int MaxPowerSampleSize = 0;
+            int MaxPower = 0;
+
+
+            for (int sampleSize = 2; sampleSize <= GridSize; sampleSize++)
+            {
+                for (int x = 1; x <= GridSize - sampleSize; x++)
+                {
+                    for (int y = 1; y <= GridSize - sampleSize; y++)
+                    {
+                        int power = GetPowerLevelWithCache(x, y, sampleSize);
+                        {
+
+                            powerCache[x, y, sampleSize] = power;
+
+                            if (power > MaxPower)
+                            {
+                                MaxPower = power;
+                                MaxPowerXCoord = x;
+                                MaxPowerYCoord = y;
+                                MaxPowerSampleSize = sampleSize;
+                            }
+                        }
+                    }
+                }
+
+                Debug.WriteLine(sampleSize);
+            }
+
+            Debug.WriteLine(MaxPowerXCoord);
+            Debug.WriteLine(MaxPowerYCoord);
+            Debug.WriteLine(MaxPowerSampleSize);
+        }
+
+        private int GetPowerLevelWithCache(int x, int y, int size)
+        {
+            int power = powerCache[x, y, size-1];
+
+            for (int a = x; a < x + size; a++)
+            {
+                power += cells[$"{a},{ y + size - 1}"].PowerLevel;
+            }
+
+            for (int b = y; b < y + size - 1 ; b++)
+            {
+                power += cells[$"{x + size -1},{b}"].PowerLevel;
+            }
+            return power;
+        }
+
+
+        private int GetPowerLevel(int x, int y, int size)
+        {                                      
+            int power = 0;
+
+            for (int a = x; a < x + size; a++)
+            {
+                for (int b = y; b < y + size; b++)
+                {
+                    power += cells[$"{a},{b}"].PowerLevel;
+                }
+            }
+            return power;
+        }
+
+        private void InitCells()
+        {
+            for (int x = 1; x <= GridSize; x++)
+            {
+                for (int y = 1; y <= GridSize; y++)
+                {
+                    var cell = new FuelCell
+                    {
+                        RackID = x + 10,
+                        X = x, 
+                        Y = y
+                    };
+
+                    cell.PowerLevel = (cell.RackID * y) + Input;
+                    cell.PowerLevel = cell.PowerLevel * cell.RackID;
+                    cell.PowerLevel = cell.PowerLevel < 99 ? 0 : Math.Abs(cell.PowerLevel / 100 % 10);
+                    cell.PowerLevel -= 5;
+                    cell.Id = $"{x},{y}";
+
+                    cells.Add(cell.Id, cell);
+
+                    powerCache[x, y, 1] = cell.PowerLevel;
+                }
+            }
         }
     }
+
+    public class FuelCell
+    {
+        public int RackID { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int PowerLevel { get; set; }
+
+        public string Id { get; set; } 
+    }    
 }
+
