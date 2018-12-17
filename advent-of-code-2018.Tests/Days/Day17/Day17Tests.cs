d46704d3acb10289c4ce2dd7c6ac95929c2d31dd
Gareth Spring <gareth.spring@dunnhumby.com>
2018-12-17 12:56:53 +0000

    Day 17

---
+++ advent-of-code-2018.Tests/Days/Day17/Day17Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using advent_of_code_2018.Days.Day17;
+
+namespace advent_of_code_2018.Tests.Days.Day17
+{
+    [TestClass]
+    public class Day17Tests
+    {
+        [TestMethod]
+        public void Day17_TestPart1()
+        {
+            var solution = new Solution();
+
+            var result = solution.Part1();
+
+            // Not 35851 
+        }
+            
+    }
+}
