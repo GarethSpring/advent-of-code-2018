bc7ad101b2bb20732dc8640b525e812493491798
Gareth Spring <gareth.spring@dunnhumby.com>
2018-12-18 15:35:02 +0000

    Day 16

---
+++ advent-of-code-2018.Tests/Days/Day16/Day16Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using advent_of_code_2018.Days.Day16;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace advent_of_code_2018.Tests.Days.Day16
+{
+   
+
+    [TestClass]
+    public class Day16Tests
+    {
+        [TestMethod]
+        public void Day16_TestPart1()
+        {
+            var solution = new Solution();
+
+            var result = solution.Part1();
+        }        
+    }
+
+
+    public class Register
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ advent-of-code-2018/Days/Day16/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2018.Days.Day16
+{
+    public class Solution
+    {
+        private Dictionary<string, Func<int, int, int>> opCodes = new Dictionary<string, Func<int, int, int>>
+        {
+            //new KeyValuePair<string, Func<int, int, int int>>("addr", Addr)
+        };
+
+        private Register rA = new Register();
+        private Register rB = new Register();
+        private Register rC = new Register();
+        private Register rD = new Register();
+
+        private int result;
+
+        public string Part1()
+        {
+            opCodes.Add("addr", Addr);
+            return null;
+        }
+
+        private int Addr(int a, int b)
+        {
+            rC.Value = rA.Value + rB.Value;
+
+            return rC.Value;
+        }
+
+        private int Addi(int a, int b)
+        {
+            rC.Value = rA.Value + b;
+
+            return rC.Value;
+        }
+
+    }
+
+
+    public class Register
+    {
+        public int Value { get; set; }
+    }
+}
